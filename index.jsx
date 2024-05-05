71c7e3a7083d1cbbf28df392f3a0d7e4b0797de3
Parabjeet44 <154970897+Parabjeet44@users.noreply.github.com>
2024-05-05 16:50:22 +0530

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+import PopBlog from './PopBlog';
+
+function Main(){
+    return(
+        <>
+            <Nav />
+            <PopBlog />
+        </>
+    );
+}
+
+export default Main;
