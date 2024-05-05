f4f348a31bf44985b5dfa114662573730e337f96
Parabjeet44 <154970897+Parabjeet44@users.noreply.github.com>
2024-05-05 17:25:51 +0530

    Create mongoDBConnect.js

---
+++ mongoDBConnect.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = 'mongodb://localhost:27017/Signup';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
